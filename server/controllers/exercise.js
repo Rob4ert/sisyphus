2f529a17448c6b962653426905625393259c559f
Isma1306 <isma1306@gmail.com>
2022-06-08 20:03:37 +0200

    Added updateExercise and reorganized routes

--- server/controllers/exercise.js
+++ server/controllers/exercise.js
@@ -38,6 +38,40 @@ const deleteExercise = async function (req, res) {
   }
 };
 
+const updateExercise = async function (req, res) {
+  const { id, userId } = req.params;
+  const { name, isWeighted, isTimed } = req.body;
+  try {
+
+    const exercise = await prisma.exercise.update({
+      where: { id: parseInt(id) },
+      data: { name, isWeighted, isTimed },
+    });
+    res.status(201);
+    res.send(exercise);
+  } catch (error) {
+    res.status(401);
+    res.send(error);
+    console.log('error :>> ', error);
+  }
+};
+
+const getExercisesByUser = async function (req, res) {
+  const { userId } = req.params;
+  try {
+    const exercises = await prisma.exercise.findMany({
+      where: { UserId: parseInt(userId) },
+    });
+    res.status(201);
+    res.send(exercises);
+  } catch (error) {
+    res.status(401);
+    res.send(error);
+    console.log('error :>> ', error);
+  }
+};
+
+
 module.exports = {
-  createExercise, deleteExercise
+  createExercise, deleteExercise, updateExercise, getExercisesByUser
 };
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
-const { createUser } = require('../controllers/user');
-const { createExercise, deleteExercise } = require('../controllers/exercise');
+const { createUser } = require('../controllers/user.controller');
+const { createExercise, deleteExercise, updateExercise, getExercisesByUser } = require('../controllers/exercise');
 
 const router = new Router();
 
@@ -10,7 +10,8 @@ router.post('/', createUser);
 // exercises
 router.post('/:userId/exercise', createExercise);
 router.delete('/:userId/exercise/:id', deleteExercise);
-
+router.put('/:userId/exercise/:id', updateExercise);
+router.get('/:userId/exercise/', getExercisesByUser);
 
 
 
