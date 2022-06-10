3cda123a8afd602acd357884e80dfc400f867b1f
Isma1306 <isma1306@gmail.com>
2022-06-10 16:28:26 +0200

    Removed authguard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
 import { LoginCardComponent } from './login-card/login-card.component';
 import { SignupCardComponent } from './signup-card/signup-card.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  {
-    path: 'app',
-    canActivateChild: [AuthGuard],
-    children: [
-      { path: 'signup', component: SignupCardComponent },
-      { path: 'login', component: LoginCardComponent },
-    ]
-  }
+  { path: 'signup', component: SignupCardComponent },
+  { path: 'login', component: LoginCardComponent },
 ];
 
 @NgModule({
