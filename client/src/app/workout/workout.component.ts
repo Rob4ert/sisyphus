2b3c515f1af562252b2f15a25ce2262031a0a706
Isma1306 <isma1306@gmail.com>
2022-06-14 15:01:00 +0200

    Added logic to get workout in workout component

--- client/src/app/workout/workout.component.ts
+++ client/src/app/workout/workout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Day, Routine } from '../interfaces';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-workout',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkoutComponent implements OnInit {
 
-  constructor() { }
+
+  public user: any;
+  public activeRoutine: any;
+
+  public baseDate = new Date(Date.now());
+  public routineDay: any;
+
+  constructor(private userService: UserService,) { }
 
   ngOnInit(): void {
+    this.userService.currentUser.subscribe(user => {
+      if (user) {
+        this.user = user;
+      }
+    });
+    this.userService.activeRoutine.subscribe((routine) => {
+      if (routine) {
+        this.activeRoutine = routine;
+        this.setRoutineDay(routine);
+      }
+    });
   }
 
+
+  setRoutineDay(routine: Routine) {
+    routine.days.forEach((routineDay: Day) => {
+      if (routineDay.weekDays.includes(this.baseDate.getDay())) {
+        this.routineDay = routineDay;
+      }
+    });
+  }
 }
+
+
+
