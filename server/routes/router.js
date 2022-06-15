d27ff3592cc2347918c025edec84075ac525e515
Isma1306 <isma1306@gmail.com>
2022-06-15 19:21:15 +0200

    Cleaned the backend routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,7 @@
 const { Router } = require('express');
 const { checkLoggedIn, checkIsUser } = require('../middlewares/auth.middleware');
 const { createUser, login, logout, getUser } = require('../controllers/user.controller');
-const { createExercise, deleteExercise, updateExercise, getExercisesByUser } = require('../controllers/exercise.controller');
 const { createRoutine, deleteRoutine, updateRoutines, getRoutinesByUser } = require('../controllers/routine.controller');
-const { createSet, deleteSet, updateSet, getSetsByDay } = require('../controllers/set.controller');
-// const { getAllDays } = require('../controllers/days.controller');
 const { signInValidator, signInConfig, logInConfig, logInValidator } = require('../middlewares/validators.middleware');
 
 const router = new Router();
@@ -15,11 +12,7 @@ router.post('/login', logInConfig, logInValidator, login);
 router.get('/login', getUser);
 router.post('/logout', checkLoggedIn, logout);
 
-// exercises
-// router.post('/:userId/exercise', checkLoggedIn, checkIsUser, createExercise);
-// router.delete('/:userId/exercise/:id', checkLoggedIn, checkIsUser, deleteExercise);
-// router.put('/:userId/exercise/:id', checkLoggedIn, checkIsUser, updateExercise);
-// router.get('/:userId/exercise/', checkLoggedIn, checkIsUser, getExercisesByUser);
+
 
 // routine
 router.post('/routine', checkLoggedIn, createRoutine);
@@ -27,15 +20,8 @@ router.put('/routine', checkLoggedIn, updateRoutines);
 router.delete('/routine', checkLoggedIn, deleteRoutine);
 router.get('/routine', checkLoggedIn, getRoutinesByUser);
 
-// days
 
-// router.get('/routine/:routineId', checkLoggedIn, getAllDays);
 
-// sets
-router.post('/:userId/set', checkLoggedIn, checkIsUser, createSet);
-router.delete('/:userId/set/:id', checkLoggedIn, checkIsUser, deleteSet);
-router.put('/:userId/set/:id', checkLoggedIn, checkIsUser, updateSet);
-router.get('/:userId/set/', checkLoggedIn, checkIsUser, getSetsByDay);
 
 
 module.exports = {
