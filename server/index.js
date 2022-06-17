249053798dbe4436a36f18424fc969c2ad2890a6
Rob4ert <rob.ryb4@gmail.com>
2022-06-17 19:07:07 +0100

    Conversion into Mobile app

--- server/index.js
+++ server/index.js
@@ -5,28 +5,27 @@ const cors = require('cors');
 const { router } = require('./routes/router');
 
 
-const PORT = process.env.PORT;
 const corsConfig = {
   origin: 'http://localhost:4200',
   credentials: true
 };
 
 const app = new Express();
-
+const PORT = 3000;
 app.use(cors(corsConfig));
 app.set('trust proxy', 1); // trust first proxy
-app.use(session({
-  name: 'sessionId',
-  secret: process.env.COOKIE_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false, // should be true when deploy
-    sameSite: 'lax',
-    httpOnly: false, // should be true when deploy
-  }
-}));
-app.use(Express.static('public'));
+// app.use(session({
+//   name: 'sessionId',
+//   secret: process.env.COOKIE_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     secure: false, // should be true when deploy
+//     sameSite: 'lax',
+//     httpOnly: false, // should be true when deploy
+//   }
+// }));
+app.use(Express.static('../client/dist'));
 app.use(Express.json());
 app.use(router);
 
