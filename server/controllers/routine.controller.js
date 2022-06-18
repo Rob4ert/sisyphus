c4ec3696b153118bec4e1c15b061f89316ec1c88
redpawner <raymondspence@hotmail.com>
2022-06-18 14:16:43 +0100

    extra

--- server/controllers/routine.controller.js
+++ server/controllers/routine.controller.js
@@ -20,58 +20,59 @@ const createRoutine = async function (req, res) {
     console.log('error :>> ', error);
   }
 };
-
-// const deleteRoutine = async function (req, res) {
-//   const { id } = req.body;
-//   try {
-//     const routine = await eraseRoutine(id);
-//     res.status(202);
-//     res.send({ error: null, data: routine });
-//   } catch (error) {
-//     res.status(400);
-//     res.send({
-//       error: 'Error deleting routine, please try again.',
-//       data: null,
-//     });
-//     console.log('error :>> ', error);
-//   }
-// };
-
-// const updateRoutine = async function (req, res) {
-//   const routines = req.body;
-//   try {
-//     const newRoutines = await reWriteRoutines(routines);
-//     res.status(201);
-//     res.send({ error: null, data: newRoutines });
-//   } catch (error) {
-//     res.status(400);
-//     res.send({
-//       error: 'Error updating routine, please try again.',
-//       data: null,
-//     });
-//     console.log('error :>> ', error);
-//   }
-// };
-
-// const getRoutinesByUser = async function (req, res) {
-//   const userId = req.session.uid;
-//   try {
-//     const routines = await readRoutines(userId);
-//     res.status(200);
-//     res.send({ error: null, data: routines });
-//   } catch (error) {
-//     res.status(400);
-//     res.send({
-//       error: 'Error fetching routines, please try again.',
-//       data: null,
-//     });
-//     console.log('error :>> ', error);
-//   }
-// };
+const deleteRoutine = async function (req, res) {
+  // Test me
+  const userId = req.session.uid;
+  const routine = req.body;
+  try {
+    const routine = await destroyRoutine(routine, userId);
+    res.status(202);
+    res.send({ error: null, data: routine });
+  } catch (error) {
+    res.status(400);
+    res.send({
+      error: 'Error deleting routine, please try again.',
+      data: null,
+    });
+    console.log('error :>> ', error);
+  }
+};
+const updateRoutine = async function (req, res) {
+  // Test me
+  const routine = req.body;
+  const userId = req.session.uid;
+  try {
+    const newRoutines = await updateRoutine(routine, userId);
+    res.status(201);
+    res.send({ error: null, data: newRoutines });
+  } catch (error) {
+    res.status(400);
+    res.send({
+      error: 'Error updating routine, please try again.',
+      data: null,
+    });
+    console.log('error :>> ', error);
+  }
+};
+const getRoutinesByUser = async function (req, res) {
+  const userId = req.session.uid;
+  try {
+    const routines = await readRoutines(userId);
+    res.status(200);
+    res.send({ error: null, data: routines });
+  } catch (error) {
+    res.status(400);
+    res.send({
+      error: 'Error fetching routines, please try again.',
+      data: null,
+    });
+    console.log('error :>> ', error);
+  }
+};
 
 module.exports = {
   createRoutine,
-  // deleteRoutine,
-  // updateRoutines,
-  // getRoutinesByUser,
+  deleteRoutine,
+  updateRoutine,
+  getRoutinesByUser,
 };
